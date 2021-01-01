f4f481ae54c7ea1873e2c02315266fa607c7dc1f
Osher <oshernati22@gmail.com>
2021-01-01 21:25:14 +0200

    osher homepage

--- client/src/Homepage/homepage.js
+++ client/src/Homepage/homepage.js
@@ -1,11 +1,19 @@
 import './homepage.scss';
 import video from '../Videos/Background.mp4'
-import SearchPage from '../Components/Searchbar/searchbar.js'
-import Stockbar from '../Components/Searchbar/Stockbar';
+import SearchPage from '../Components/searchbar.js'
+import Stockbar from '../Components/Stockbar';
+import Navbar from '../Components/Navbar';
+import Footer from '../Components/Footer';
+import img from '../Videos/logoinvestock.png';
+import stockslist from "../Components/stocksForSearch";
 
 function Homepage() {
   return (
+    <>
     <body>
+      <div className="logo-box">
+               <img src={img} alt="Logo" class="logo"/>
+           </div>
     <div className="Homepage">
       <header className="Homepage-header">
         <div className="bg-video">
@@ -13,14 +21,27 @@ function Homepage() {
                     <source src={video} type="video/mp4"/>
                 </video>  
         </div>
+        <Navbar/>
+        <div className="stockbarbox">
         <Stockbar/>
+        </div>
         <div className="searchbox">
-        <h2 className="searchbox__header">Investock</h2>
-          <SearchPage/>
+        <h2 className="searchbox__header">Inve<span>S</span>tock</h2>
+          <SearchPage items={stockslist} />
         </div>
       </header>
     </div>
     </body>
+    <footer className="footer">
+    <Footer/>
+    </footer>
+ 
+    
+    
+   
+   x
+    </>
+
   );
 }
 
