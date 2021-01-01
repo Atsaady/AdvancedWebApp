97030b4ffd850e647a3ff30feacc26abc90dad07
Maoragai12 <67845335+Maoragai12@users.noreply.github.com>
2021-01-01 22:27:12 +0200

    Merge

---
+++ client/src/Components/Footer/Footer.js
@@ -0,0 +1,65 @@
+import React from "react";
+import "./footer.scss";
+import img from "../../Videos/logoinvestock.png";
+
+function footer() {
+  return (
+    <footer className="footer">
+      <div className="footer__logo-box">
+        <div className="footer__logo">
+          <img alt="Full logo" className="footer__logo" src={img} />
+        </div>
+      </div>
+      <div className="row">
+        <div class="col-1-of-2">
+          <p class="footer__copyright">
+            אתר זה נועד למטרות לימודיות בלבד. מפתחי האתר לא מבטיחים שאתר זה מכיל
+            מידע עדכני על מניות או שהאלגורתמים יעזרו לך להיות מליונר ולהכות
+            תשוק. מפתחי האתר אינם אחראים לשום נזק שיגרם מההשקעה או משימוש במידע
+            זה ולכן מזהירים שנית לא לקחת ברצינות את הדיאטה שכתובה פה,חמי אוהבים
+            אותך יאח תן 100. מי שיהיה אמיץ ויגנוב את העיצוב אני יזיין אותו, ראו
+            הוזהרתם !!!
+          </p>
+        </div>
+        <div className="col-1-of-2">
+          <div className="footer__navigation">
+            <ul className="footer__list">
+              <li className="footer__item">
+                <a href="#" className="footer__link">
+                  אריק
+                </a>
+              </li>
+              <li className="footer__item">
+                <a href="#" className="footer__link">
+                  עברית
+                </a>
+              </li>
+              <li className="footer__item">
+                <a href="#" className="footer__link">
+                  מלך הריאקט
+                </a>
+              </li>
+              <li className="footer__item">
+                <a href="#" className="footer__link">
+                  והסטקיים
+                </a>
+              </li>
+              <li className="footer__item">
+                <a href="#" className="footer__link">
+                  מאור
+                </a>
+              </li>
+              <li className="footer__item">
+                <a href="#" className="footer__link">
+                  גלידה
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default footer;
--- client/src/Homepage/homepage.js
+++ client/src/Homepage/homepage.js
@@ -1,47 +1,44 @@
-import './homepage.scss';
-import video from '../Videos/Background.mp4'
-import SearchPage from '../Components/searchbar.js'
-import Stockbar from '../Components/Stockbar';
-import Navbar from '../Components/Navbar';
-import Footer from '../Components/Footer';
-import img from '../Videos/logoinvestock.png';
+import "./homepage.scss";
+import video from "../Videos/Background.mp4";
+import SearchPage from "../Components/Searchbar/searchbar";
+import Stockbar from "../Components/Stockbar/Stockbar";
+import Navbar from "../Components/Navbar/Navbar";
+import Footer from "../Components/Footer/Footer";
+import img from "../Videos/logoinvestock.png";
 import stockslist from "../Components/stocksForSearch";
 
 function Homepage() {
   return (
     <>
-    <body>
-      <div className="logo-box">
-               <img src={img} alt="Logo" class="logo"/>
-           </div>
-    <div className="Homepage">
-      <header className="Homepage-header">
-        <div className="bg-video">
-                <video className="bg-video__content" autoPlay muted loop >
-                    <source src={video} type="video/mp4"/>
-                </video>  
+      <body>
+        <div className="logo-box">
+          <img src={img} alt="Logo" class="logo" />
         </div>
-        <Navbar/>
-        <div className="stockbarbox">
-        <Stockbar/>
+        <div className="Homepage">
+          <header className="Homepage-header">
+            <div className="bg-video">
+              <video className="bg-video__content" autoPlay muted loop>
+                <source src={video} type="video/mp4" />
+              </video>
+            </div>
+            <Navbar />
+            <div className="stockbarbox">
+              <Stockbar />
+            </div>
+            <div className="searchbox">
+              <h2 className="searchbox__header">
+                Inve<span>S</span>tock
+              </h2>
+              <SearchPage items={stockslist} />
+            </div>
+          </header>
         </div>
-        <div className="searchbox">
-        <h2 className="searchbox__header">Inve<span>S</span>tock</h2>
-          <SearchPage items={stockslist} />
-        </div>
-      </header>
-    </div>
-    </body>
-    <footer className="footer">
-    <Footer/>
-    </footer>
- 
-    
-    
-   
-   x
+      </body>
+      <footer className="footer">
+        <Footer />
+      </footer>
+      x
     </>
-
   );
 }
 
