431afa26197896eea8d8738b70c7109e22469e4c
Osher <oshernati22@gmail.com>
2021-02-15 22:08:03 +0200

    build calculators

--- client/src/Calculator/calculator.js
+++ client/src/Calculator/calculator.js
@@ -1,8 +1,19 @@
 import React from "react";
-
+import "./calculator.scss"
+import {Container, Jumbotron,Col,Row} from "react-bootstrap";
 
 export default function Calculator() {
   return (
-        <h1>dammmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm</h1>
-  );
+    <Jumbotron className="header">
+    <Row>
+    <Col>
+    <h1 className="header__header">מחשבונים כלכליים</h1>
+    <hr/>
+    <p className="header__para">
+    .לרשותכם מחשבונים כלכליים אשר יעזרו לכם לקבל נקודת מבט נוספת לגבי שווי חברות ,חישוב רווח ועוד
+    </p>
+    </Col>
+    </Row>
+  </Jumbotron>
+    );
 }
