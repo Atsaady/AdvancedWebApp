8d6329da967a5a0a35d8e5f9b085cc4355591bfd
Osher <oshernati22@gmail.com>
2021-02-16 09:39:45 +0200

    Merge branch 'Dev' of

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -4,7 +4,7 @@ import Footer from "./Footer/Footer";
 import Navbar from "../Components/Navbar/Navbar";
 import Homepage from "../Homepage/homepage";
 import StockPage from "../StockPage/StockPage";
-import { Switch, Route,BrowserRouter } from "react-router-dom";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
 import Dictionary from "../Dictionary/dictionary";
 import Calculator from "../Calculator/calculator";
 
@@ -12,24 +12,23 @@ export default function App() {
   return (
     <main>
       <BrowserRouter>
-      <Navbar />
-      <Switch>
-        <Route exact path="/" >
-          <Homepage />
-        </Route>
-        <Route exact path="/calc">
-          <Calculator />
-        </Route>
-        <Route exact path="/dic">
-          <Dictionary />
-        </Route>
-        <Route exact path="/:stock">
-          <StockPage/>
-        </Route>
-      </Switch>
-      <Footer marginTop={"47%"} />
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Homepage />
+          </Route>
+          <Route exact path="/calc">
+            <Calculator />
+          </Route>
+          <Route exact path="/dic">
+            <Dictionary />
+          </Route>
+          <Route exact path="/:stock">
+            <StockPage />
+          </Route>
+        </Switch>
+        <Footer marginTop={"47%"} />
       </BrowserRouter>
-      
     </main>
   );
 }
